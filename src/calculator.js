5b1c29ea4cf752eee6461149e26164751ea66591
samuel <samuelgarciacompanys@gmail.com>
2019-11-13 17:57:37 +0100

    added calculator module

---
+++ src/calculator.js
@@ -0,0 +1,20 @@
+const calculator = {
+
+    addition: (a,b) => {
+        return a+b;
+    },
+
+    substraction: (a,b) => {
+        return a-b;
+    },
+
+    multiply: (a,b) => {
+        return a*b;
+    },
+
+    fraction: (a,b) => {
+        return a/b;
+    }
+};
+
+module.exports = calculator;
--- src/testing.test.js
+++ src/testing.test.js
@@ -1,5 +1,6 @@
 const mycapitalize = require('./mycapitalize');
 const myreverse = require('./myreverse');
+const calculator = require('./calculator')
 
 
 test('My capitalize gets a string and returs the same string with the first letter in uppercase', () =>{
@@ -8,4 +9,20 @@ test('My capitalize gets a string and returs the same string with the first lett
 
 test('My reverse takes a string and returns its reverse', () =>{
     expect(myreverse('hello')).toBe('olleh');
+});
+
+test('calculator can sum numbers', () =>{
+    expect(calculator.addition(1,2)).toBe(3);
+});
+
+test('calculator can substract a number from another', () =>{
+    expect(calculator.substraction(1,2)).toBe(-1);
+});
+
+test('calculator can multiply numbers', () =>{
+    expect(calculator.multiply(1,2)).toBe(2);
+});
+
+test('calculator can get a fraction', () =>{
+    expect(calculator.fraction(1,2)).toBe(0.5);
 });
