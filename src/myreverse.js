84ba61f76d0a4cffe99d459a6ff8564b11ecbe11
samuel <samuelgarciacompanys@gmail.com>
2019-11-14 16:10:02 +0100

    myreverse

--- src/myreverse.js
+++ src/myreverse.js
@@ -1,5 +1,10 @@
 const myreverse = (str) => {
     
+    result = []
+    for(let i = str.length-1; i >= 0; i--){
+        result.push(str[i])
+    }
+    return result.join('');
 }
 
 module.exports = myreverse;
